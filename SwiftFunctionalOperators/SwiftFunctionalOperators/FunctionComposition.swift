269ff312b78d2842e1e010c427e40b4a62eefed1
Fernando Luiz Goulart <fernando@leapi.com>
2022-07-13 19:12:52 -0300

    FunctionComposition operator

---
+++ SwiftFunctionalOperators/SwiftFunctionalOperators/FunctionComposition.swift
@@ -0,0 +1,21 @@
+//
+//  FunctionComposition.swift
+//  SwiftFunctionalOperators
+//
+//  Created by Fernando Luiz Goulart on 13/07/22.
+//
+
+import Foundation
+
+infix operator >>>: ForwardComposition
+
+precedencegroup ForwardComposition {
+    associativity: left
+    higherThan: ForwardApplication
+}
+
+func >>> <A,B,C>(f: @escaping (A)->B, g: @escaping (B)->C) -> ((A)-> C) {
+    return { a in
+        g(f(a))
+    }
+}
